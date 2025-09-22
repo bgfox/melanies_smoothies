91985437fef97e3400a67a3fa3619ec9401102cb
bgfox <78603916+bgfox@users.noreply.github.com>
2025-09-22 16:38:01 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,6 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
       ingredients_string += fruit_chosen +' '
-      
       search_on=pd.df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
       st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
       
