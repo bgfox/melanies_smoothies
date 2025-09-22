bf38873de692091a50b74b93226f8494072703eb
bgfox <78603916+bgfox@users.noreply.github.com>
2025-09-22 15:58:52 +1000

    Update streamlit_app.py
    Exposing JSON data in response object

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-# New section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -64,7 +61,10 @@ if ingredients_list:
        st.success('Your smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
     
-
+# New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json())
 
    
 
